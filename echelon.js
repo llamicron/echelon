c9c93c517d858c3bc9ab87aecd9e1747713015e3
Luke Sweeney <luke@thesweeneys.org>
2018-04-03 19:37:24 -0500

    Initial Commit

---
+++ echelon.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+  el: "#main",
+  data: {
+    matrices: [
+      [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+      [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
+      [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+    ],
+    matrixInput: "",
+    error: ""
+  },
+
+  methods: {
+    addMatrix() {
+      matrix = this.getMatrix();
+    },
+
+    getMatrix() {
+      // example input:
+      // 2x + 3y - 7z = 2
+      // x + 4y - 2z = 1
+      // 7x + 4y - 2z = 7
+      eqs = this.matrixInput.split('\n'); // One equation per line
+      eqs = eqs.filter(function (n) { return n != "" }); // Delete empty strings
+
+      // Ensure there are 3 and only 3 equations
+      if (eqs.length != 3) {
+        this.error = "You need to provide 3 equations";
+        return false;
+      } else {
+        this.error = '';
+      }
+
+      // eqs = eqs.slice(0, 3);
+
+      console.log(eqs);
+    }
+  }
+})
+
+
